de0d403716e445040691f72c1785121bbdb6c8b4
biozuchi <mazuchi.augusto@hotmail.com>
2024-12-22 17:27:39 -0300

    requisito 01

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -1,5 +1,7 @@
 package com.trybe.java.regraprogressao;
 
+import java.util.Scanner;
+
 /**
  * App.
  */
@@ -9,5 +11,21 @@ public class App {
    * Metodo main.
    */
   public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    String input;
+
+    System.out.println("Digite a quantidade de atividades para cadastrar:");
+    input = scn.nextLine();
+    int qtdAtividades = Integer.parseInt(input);
+
+    for (int i = 1; i <= qtdAtividades; i++) {
+      System.out.println("Digite o nome da atividade " + i + ":");
+      String nome = scn.nextLine();
+      System.out.println("Digite o peso da atividade " + i + ":");
+      input = scn.nextLine();
+      int peso = Integer.parseInt(input);
+    }
+
+    scn.close();
   }
 }
