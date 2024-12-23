82990e47f388a56eb909fb1a2c830dec5a459d35
biozuchi <mazuchi.augusto@hotmail.com>
2024-12-23 19:11:17 -0300

    requisito 04

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -1,6 +1,7 @@
 package com.trybe.java.regraprogressao;
 
 import java.util.Scanner;
+import javax.sound.midi.Soundbank;
 
 /**
  * App.
@@ -13,22 +14,28 @@ public class App {
   public static void main(String[] args) {
     Scanner scn = new Scanner(System.in);
     String input;
+    String nome;
 
     System.out.println("Digite a quantidade de atividades para cadastrar:");
     input = scn.nextLine();
+
     int qtdAtividades = Integer.parseInt(input);
     int[] pesos = new int[qtdAtividades];
+    int[] notas = new int[qtdAtividades];
 
     for (int i = 1; i <= qtdAtividades; i++) {
       System.out.println("Digite o nome da atividade " + i + ":");
-      String nome = scn.nextLine();
+      nome = scn.nextLine();
+
       System.out.println("Digite o peso da atividade " + i + ":");
       input = scn.nextLine();
       int peso = Integer.parseInt(input);
+      pesos[i - 1] = peso;
+
       System.out.println("Digite a nota obtida para " + nome + ":");
       input = scn.nextLine();
       int nota = Integer.parseInt(input);
-      pesos[i - 1] = nota;
+      notas[i - 1] = nota;
     }
 
     int total = 0;
@@ -38,8 +45,23 @@ public class App {
 
     if (total != 100) {
       System.out.println("A soma dos pesos é diferente de 100!");
+    } else {
+      double divisor = 0;
+      double dividendo = 0;
+      for (int i = 0; i < pesos.length; i++) {
+        divisor += (pesos[i] * notas[i]);
+        dividendo += pesos[i];
+      }
+      double resultado = divisor / dividendo;
+      String pref = "Lamentamos informar que, com base na sua pontuação alcançada neste período, ";
+      String suf = "%, você não atingiu a pontuação mínima necessária para sua aprovação.";
+      String msg = "%! E temos o prazer de informar que você obteve aprovação!";
+      if (resultado < 85.0) {
+        System.out.println(pref + resultado + suf);
+      } else {
+        System.out.println("Parabéns! Você alcançou " + resultado + msg);
+      }
     }
-
     scn.close();
   }
 }
